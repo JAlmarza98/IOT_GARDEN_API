fe5e01bbb7565a1a6167358abdd53c505b5cbff3
JAlmarza98 <j.almarza98@gmail.com>
2023-03-13 21:48:27 +0100

    ➕ Conexión con base de datos

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,21 @@
-import {app} from "./app";
+import {app} from './app';
+import * as dotenv from 'dotenv';
+import cors from 'cors';
+import Database from './lib/database';
 
-const port = 5000;
+dotenv.config()
+
+// Middlewares
+app.use(cors());
+
+const database = new Database();
+database.init();
+
+const port = process.env.PORT ? process.env.PORT : 5000;
 
 // Start server
-app.listen(port, () => console.log(`Server is listening on port ${port}!`));
+app.listen(port, () => {
+    console.log('================API================');
+    console.log(`Server runnig on port ${port}`);
+    console.log('===================================');
+});
